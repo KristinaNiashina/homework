662ebc04b475d774e8a59a988dc26142bac54c9f
Kristina <skristina448@gmail.com>
2023-03-09 19:51:31 +0500

    Final commit

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,90 @@
+//TASK ONE
+
 function getArrayParams(...arr) {
+  let min = Infinity;
+  let max = -Infinity;
+  let avg = 0;
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++){
+    if (min > arr[i]){
+      min = arr[i];
+    }
+    if (max < arr[i]){
+      max = arr[i];
+    }
+    sum += arr[i];
+  }
+  avg = Number((sum/arr.length).toFixed(2));
 
   return { min: min, max: max, avg: avg };
 }
 
-function summElementsWorker(...arr) {
 
+//TASK TWO
+
+
+function summElementsWorker(...arr) {
+  if (!arr.length > 0) { return 0; }
+  
+  let sum = arr.reduce((currentSum, currentNumber) => {
+    return currentSum + currentNumber 
+  }, 0)
+  return sum;
 }
 
-function differenceMaxMinWorker(...arr) {
 
+function differenceMaxMinWorker(...arr) {
+  if (!arr.length > 0) { return 0; }
+
+  let max = Math.max(...arr); 
+  let min = Math.min(...arr);
+
+  return max - min;
 }
 
 function differenceEvenOddWorker(...arr) {
+  if (!arr.length > 0) { return 0; }
 
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] % 2 == 0){
+      sumEvenElement += arr[i];
+    } else {
+      sumOddElement += arr[i];
+    }
+  }
+  return sumEvenElement - sumOddElement;
 }
 
 function averageEvenElementsWorker(...arr) {
+  if (!arr.length > 0) { return 0; }
 
+  let sumEvenElement = 0;
+  let countEvenElement = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if(arr[i] % 2 == 0){
+      sumEvenElement += arr[i];
+      countEvenElement++;
+    }
+  } 
+  return sumEvenElement / countEvenElement;
 }
+
 
 function makeWork (arrOfArr, func) {
+  let maxWorkResult = -Infinity;
 
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let max = func(...arrOfArr[i]);
+    if(maxWorkResult < max){
+      maxWorkResult = max;
+    }
+  }
+  return maxWorkResult;
 }
+
+//Не совсем поняла как можно передать минимальный элемент из самой первой функции
